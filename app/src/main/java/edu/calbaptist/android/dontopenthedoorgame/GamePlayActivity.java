dd4e6d3f8ca3879afcf6e4fce69e66f8bf6a9020
Charlie Davidson <charliedavidson.shs@gmail.com>
2017-10-28 00:17:05 -0700

    worked on classes

--- app/src/main/java/edu/calbaptist/android/dontopenthedoorgame/GamePlayActivity.java
+++ app/src/main/java/edu/calbaptist/android/dontopenthedoorgame/GamePlayActivity.java
@@ -1,13 +1,53 @@
 package edu.calbaptist.android.dontopenthedoorgame;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class GamePlayActivity extends AppCompatActivity {
+public class GamePlayActivity extends SurfaceView implements SurfaceHolder.Callback {
+
+    private GameThread thread;  // Initialize GameThread
+
+    /**
+     * Constructor
+     * @param context
+     */
+    public GamePlayActivity(Context context) {
+        super(context);
+
+        getHolder().addCallback(this);  // Callback is set to the SurfaceView so events get triggered from here
+
+        thread = new GameThread(getHolder(), this);
+
+        setFocusable(true);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_play);
+    public void surfaceCreated(SurfaceHolder holder) {
+        thread.setRunning(true);    // When surface is created thread is set to true
+        thread.start();             // Starts thread
+    }
+
+    @Override
+    public void surfaceChanged (SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        while (retry) {
+            try{
+                thread.setRunning(false);
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            retry = false;
+        }
+    }
+
+    public void update() {
+
     }
 }
--- app/src/main/java/edu/calbaptist/android/dontopenthedoorgame/HomeScreenMainActivity.java
+++ app/src/main/java/edu/calbaptist/android/dontopenthedoorgame/HomeScreenMainActivity.java
@@ -1,13 +1,24 @@
 package edu.calbaptist.android.dontopenthedoorgame;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class HomeScreenMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen_main);
+        super.onCreate(savedInstanceState);             // App is created
+
+        setContentView(R.layout.activity_home_screen_main); // Brand new surface view to render game rather than xml
+    }
+    public void PlayButtonClick(View view) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);  // Removes title
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);    // Makes game fullscreen
+        setContentView(new GamePlayActivity(this)); // Brand new surface view to render game rather than xml
+
     }
 }
