6bf85ee468897056a355ab068bcb0838b1e71731
Charlie Davidson <charliedavidson.shs@gmail.com>
2017-11-07 15:42:05 -0800

    added some xml, etc

--- app/src/main/java/edu/calbaptist/android/dontopenthedoorgame/GamePlayActivity.java
+++ app/src/main/java/edu/calbaptist/android/dontopenthedoorgame/GamePlayActivity.java
@@ -1,7 +1,12 @@
 package edu.calbaptist.android.dontopenthedoorgame;
 
 import android.app.Activity;
+import android.widget.Button;
 
 public class GamePlayActivity extends Activity {
+
+    Button mDialogNewScore = (Button) findViewById(R.id.new_score_test);
+    //@Override
+    //public void onClick(View)
 
 }
